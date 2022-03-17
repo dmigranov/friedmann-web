2ded49e1d6eeffb1c3f00d94e2adc65243bd5f86
dmigranov <d.migranov@g.nsu.ru>
2022-03-17 16:24:01 +0700

    some

--- friedmann.ts
+++ friedmann.ts
@@ -14,10 +14,11 @@ function main () {
       return;
     }
   
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    drawScene(gl, programInfo, buffers);
   }  
+
+  function drawScene(gl, programInfo, buffers) {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  }
 
   window.onload = main;
