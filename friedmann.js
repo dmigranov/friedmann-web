74dcc5dbecb37b8d9769d8bb823eedc40a228f81
dmigranov <d.migranov@g.nsu.ru>
2022-03-17 19:09:59 +0700

    some

--- friedmann.js
+++ friedmann.js
@@ -33,7 +33,9 @@ function main() {
 
   const fsSource = `
   void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0); // initially the same
+
+    //todo: modify
   }
 `;
 
