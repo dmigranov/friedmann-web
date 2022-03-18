adc97f2a39a46c3e31ada74991e3a55334a3fdd3
dmigranov <d.migranov@g.nsu.ru>
2022-03-18 10:34:56 +0700

    some

--- friedmann.js
+++ friedmann.js
@@ -21,10 +21,6 @@ function main() {
   // do one-time initialization of graphics resources
   //var scene = init(gl);
 
-  // Initialize a shader program; this is where all the lighting
-  // for the vertices and so forth is established.
-  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-
   // Collect all the info needed to use the shader program.
   // Look up which attribute our shader program is using
   // for aVertexPosition and look up uniform locations.
@@ -132,7 +128,6 @@ function init(gl) {
   }`;
   
   // Fragment shader program
-
   const fsSource = `
   void main() {
     gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0); // initially the same
@@ -140,7 +135,8 @@ function init(gl) {
     //todo: modify
   }`;
 
-  
+  // Initialize a shader program; this is where all the lighting
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 }
 
 //
