ddf1079b80f6bd217e36317a5e101df4e5297fd1
dmigranov <d.migranov@g.nsu.ru>
2022-03-18 10:51:00 +0700

    some cleaning

--- friedmann.js
+++ friedmann.js
@@ -1,6 +1,4 @@
-// https://armno.medium.com/vscode-and-webgl-development-dfc17bba52ed
-
-// у меня будет своя математика... тогда ошибок не будет...
+// todo: WebGL поддерживает Instancing!
 
 window.onload = main;
 
@@ -102,7 +100,6 @@ function render(gl, programInfo, buffers) {
   }
 }
 
-
 function init(gl) {
   // Vertex shader program
   const vsSource = `
@@ -144,20 +141,17 @@ function init(gl) {
 //
 // Initialize a shader program, so WebGL knows how to draw our data
 //
-
 function initShaderProgram(gl, vsSource, fsSource) {
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
   const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
   // Create the shader program
-
   const shaderProgram = gl.createProgram();
   gl.attachShader(shaderProgram, vertexShader);
   gl.attachShader(shaderProgram, fragmentShader);
   gl.linkProgram(shaderProgram);
 
   // If creating the shader program failed, alert
-
   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
     alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
     return null;
@@ -166,11 +160,8 @@ function initShaderProgram(gl, vsSource, fsSource) {
   return shaderProgram;
 }
 
-
 //
-// initBuffers
-//
-// Initialize the buffers we'll need. 
+// Initialize the buffers we will need. 
 //
 function initBuffers(gl) {
   // Create a buffer for the square's positions.
@@ -188,9 +179,7 @@ function initBuffers(gl) {
     -1.0, -1.0,
   ];
 
-  // Now pass the list of positions into WebGL to build the
-  // shape. We do this by creating a Float32Array from the
-  // JavaScript array, then use it to fill the current buffer.
+  // Now pass the list of positions into WebGL to build the shape. 
   gl.bufferData(gl.ARRAY_BUFFER,
     new Float32Array(positions),
     gl.STATIC_DRAW);
@@ -202,22 +191,18 @@ function initBuffers(gl) {
 
 
 //
-// creates a shader of the given type, uploads the source and
-// compiles it.
+// creates a shader of the given type, uploads the source and compiles it.
 //
 function loadShader(gl, type, source) {
   const shader = gl.createShader(type);
 
   // Send the source to the shader object
-
   gl.shaderSource(shader, source);
 
   // Compile the shader program
-
   gl.compileShader(shader);
 
   // See if it compiled successfully
-
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
@@ -226,5 +211,3 @@ function loadShader(gl, type, source) {
 
   return shader;
 }
-
-// todo: WebGL поддерживает Instancing
