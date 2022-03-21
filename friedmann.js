ad3c76b98f0d0f18624eb87731a8d2fd5f38c074
dmigranov <d.migranov@g.nsu.ru>
2022-03-21 19:01:34 +0700

    sphericalRotationXW implemented and working

--- friedmann.js
+++ friedmann.js
@@ -183,7 +183,7 @@ function updatePage(scene, deltaTime) {
 
   const transformed = vec4.fromValues(0, 0, 0, 1);
   if (leftMouseButtonPressed)
-    output2.innerHTML = vec4.str(vec4.transformMat4(transformed, transformed, sphericalRotationZW(0.1)));
+    output2.innerHTML = vec4.str(vec4.transformMat4(transformed, transformed, sphericalRotationXW(0.1)));
   else
     output2.innerHTML = "Mouse up!";
 
@@ -579,23 +579,33 @@ function rightMouseClickHandler(event) {
 /* 
 function bananaProjectionMatrixFrontHalf(fovY, aspect, z0)
 {
-	const height = 1 / Math.tan(fovY / 2);
-	const width = height / aspect;
-	return Matrix(width, 0., 0., 0.,
-		0., height, 0., 0.,
-		0., 0., 0.25, 1.,
-		0., 0., -z0 / 4., 0);
+  const height = 1 / Math.tan(fovY / 2);
+  const width = height / aspect;
+  return Matrix(
+    width, 0., 0., 0.,
+    0., height, 0., 0.,
+    0., 0., 0.25, 1.,
+    0., 0., -z0 / 4., 0);
 }
 */
 
 // rotation around plane XY which stays invariant
 function sphericalRotationZW(d) // знаки поменял в сравнении с DX - проверить!
-{
-  //from values: 
-	return mat4.fromValues(	1, 0, 0, 0,
-					0, 1, 0, 0,
-					0, 0, Math.cos(d), -Math.sin(d),
-					0, 0, Math.sin(d), Math.cos(d));
+{ 
+  return mat4.fromValues(
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, Math.cos(d), -Math.sin(d),
+    0, 0, Math.sin(d), Math.cos(d));
+}
+
+function sphericalRotationXW(d) // знаки поменял в сравнении с DX - проверить!
+{ 
+  return mat4.fromValues(
+    Math.cos(d), 0, 0, -Math.sin(d),
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    Math.sin(d), 0, 0, Math.cos(d));
 }
 
 //todo: остальные также
