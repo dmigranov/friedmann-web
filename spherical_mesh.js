64f8bdda37b2072981c73e661055c66f5ba90217
dmigranov <d.migranov@g.nsu.ru>
2022-03-25 16:48:17 +0700

    some porting

--- spherical_mesh.js
+++ spherical_mesh.js
@@ -14,14 +14,35 @@ export function createSphere(gl, radius, sliceCount, stackCount, color) {
 
 	// North Pole
 	positions.push(0., radius, 0., 1);	//todo: заменить 1 на height
-	colors.concat(color);	
+	colors.concat(color);
 	console.log(colors);
 	tex.push(0., 0.);
+
+	for (var i = 1; i <= stackCount - 1; i++) {
+		const phi = i * phiStep;
+		for (var j = 0; j <= sliceCount; j++) {
+			const theta = j * thetaStep;
+            XMFLOAT4 pos(
+				(radius * sinf(phi) * cosf(theta)),
+				(radius * cosf(phi)),
+				(radius * sinf(phi) * sinf(theta)),
+				height
+			);
+
+            const uv = XMFLOAT2(theta / XM_2PI, phi / XM_PI);
+			vertices.push_back({
+				pos,
+				//pos,       //так как это сфера! нормализовывать не нужно, так как и так радиус пространства 1 (но если сферическое пространство произвольного радиуса, то нужно)
+				uv
+			});
+
+		}
+	}
 
 
 	// South Pole
 	positions.push(0., -radius, 0., 1);	//todo: заменить 1 на height
-	colors.concat(color);	
+	colors.concat(color);
 	console.log(colors);
 	tex.push(0., 1.);
 
