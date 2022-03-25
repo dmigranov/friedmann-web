e8491bf269bf91084866d127492fc1bb65849d72
dmigranov <d.migranov@g.nsu.ru>
2022-03-25 16:15:53 +0700

    some work on createSphere func

--- spherical_mesh.js
+++ spherical_mesh.js
@@ -5,9 +5,22 @@ export function createSphere(gl) {
 	var indices = [];
 
 
-	const positionBuffer = gl.createBuffer(); 
+	const positionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-	
+	const colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+	const indexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, //! for indices we use this
+		new Uint16Array(indices), gl.STATIC_DRAW);
+
+	return {
+		position: positionBuffer,
+		color: colorBuffer,
+		indices: indexBuffer,
+	};
 }
