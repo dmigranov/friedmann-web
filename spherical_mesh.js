45d354d54f7db2166ea8ff803811a9eb96b4b87d
dmigranov <d.migranov@g.nsu.ru>
2022-03-25 16:13:54 +0700

    some

--- spherical_mesh.js
+++ spherical_mesh.js
@@ -1,6 +1,13 @@
 
-export function createSphere() {
+export function createSphere(gl) {
 	var positions = [];
 	var colors = [];
-	var indices [];
+	var indices = [];
+
+
+	const positionBuffer = gl.createBuffer(); 
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+	
 }
