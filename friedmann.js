9e3fde3e2b291844f31ea5308c5033997c3a1751
dmigranov <d.migranov@g.nsu.ru>
2022-03-26 18:06:09 +0700

    вращение камеры клавиатурой

--- friedmann.js
+++ friedmann.js
@@ -92,7 +92,8 @@ function updateScene(scene, deltaTime) {
 	// just modify it!
 
 	{ //that's option 1
-		const worldMatrix = mat4.create();
+		const viewMatrix = mat4.create();
+
 		if (leftPressed) {
 			cubeRotationX -= deltaTime;
 		}
@@ -106,9 +107,12 @@ function updateScene(scene, deltaTime) {
 			cubeRotationY -= deltaTime;
 		}
 
-		mat4.rotate(worldMatrix, worldMatrix, cubeRotationX, [0, 1, 0]);
-		mat4.rotate(worldMatrix, worldMatrix, cubeRotationY, [1, 0, 0]);
-		constants.worldMatrix = worldMatrix;
+		mat4.rotate(viewMatrix, viewMatrix, cubeRotationX, [0, 1, 0]);
+		mat4.rotate(viewMatrix, viewMatrix, cubeRotationY, [1, 0, 0]);
+		mat4.translate(viewMatrix, viewMatrix, [-0.0, 0.0, -6.0]);
+
+
+		constants.viewMatrixFront = viewMatrix;
 	}
 }
 
