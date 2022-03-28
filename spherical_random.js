b038313c885fb0207b760ce7ba477992165ce088
dmigranov <d.migranov@g.nsu.ru>
2022-03-28 16:33:18 +0700

    generating four numbers (uniform)

--- spherical_random.js
+++ spherical_random.js
@@ -1,8 +1,17 @@
 // js random ~ uniform distribution (Euclidean)
 
+function getRandom(min, max) {
+	return Math.random() * (max - min) + min;
+  }
+
 export function generatePoint(spaceRadius) {
 	var isPointGenerated = false;
     while (!isPointGenerated) {
+		const x = getRandom(-spaceRadius, spaceRadius);
+		const y = getRandom(-spaceRadius, spaceRadius);
+		const z = getRandom(-spaceRadius, spaceRadius);
+		const w = getRandom(-spaceRadius, spaceRadius);
+
 		
 	}
 }
