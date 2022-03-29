260e33bf57ba0e8ad987d7dc47832a851ee72dc3
dmigranov <d.migranov@g.nsu.ru>
2022-03-29 19:52:33 +0700

    some

--- friedmann-timer.js
+++ friedmann-timer.js
@@ -2,11 +2,17 @@
 class FriedmannTimer {
 	constructor(initialSimulationTime, muCoeff) {
 		this.currentSimulationTime = initialSimulationTime;
-
+		this.mu = muCoeff * this.currentSimulationTime;
 		this.muCoeff = muCoeff;
 	}
 
 	get simulationTime() {
 		return this.currentSimulationTime;
 	}
+
+	addDelta(deltaTime) {
+		m_currentSimulationTime += deltaTime;
+    m_currentFrameTime += deltaTime;
+    m_mu += m_muCoeff * deltaTime;
+	  }
 }
