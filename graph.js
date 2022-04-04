8d2768418fdd071602378763c1c830a947c58485
dmigranov <d.migranov@g.nsu.ru>
2022-04-04 14:09:42 +0700

    border gap var

--- graph.js
+++ graph.js
@@ -1,19 +1,20 @@
+const borderGap = 10;
+
 export function drawAxes(graphCanvas, context2d) {
     context2d.save();
-    context2d.lineWidth = 2;
+    context2d.lineWidth = 1;
 
     // +Y axis
     context2d.beginPath();
-    context2d.moveTo(10, 10);
-    context2d.lineTo(10, graphCanvas.height - 10);
+    context2d.moveTo(borderGap, borderGap);
+    context2d.lineTo(borderGap, graphCanvas.height - borderGap);
     context2d.stroke();
 
     // +X axis
-    /*
     context2d.beginPath();
     context2d.moveTo(XC(0), YC(0));
     context2d.lineTo(XC(MaxX()), YC(0));
     context2d.stroke();
-    */
+    
     context2d.restore();
 }
