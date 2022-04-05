aeb9b5c91e1678e1a0fe6cef1c395e420c1e6534
dmigranov <d.migranov@g.nsu.ru>
2022-04-05 16:48:54 +0700

    some

--- friedmann.js
+++ friedmann.js
@@ -244,7 +244,7 @@ function updateScene(scene, deltaTime) {
 		else {
 			// mouseX = (double)ms.x / width * 2. - 1.;
 			// mouseY = -((double)ms.y / height * 2. - 1);
-			SelectionSystem.raytraceSphereMouse(0, 0, sphPosition, spaceRadius, projectionMatrix);
+			SelectionSystem.raytraceSphereMouse(0, 0, sphPosition, spaceRadius, projectionMatrix, radiusFunction);
 		}
 	}
 }
--- selection_system.js
+++ selection_system.js
@@ -1,8 +1,18 @@
-export function raytraceSphereMouse(mouseX, mouseY, posView, spaceRadius, projMatrix) {
-	const pos = posView; 
-	var chi = Math.acos(pos.w / spaceRadius);
-    if (pos.z < 0)
-        chi = 2 * Math.PI - chi;
-	//console.log(chi);
+//pos[3] = pos.w
+//pos[2] = pos.z
 
+export function raytraceSphereMouse(mouseX, mouseY, posView, spaceRadius, projMatrix, radiusFunction) {
+	const pos = posView; 
+	var chi = Math.acos(pos[3] / spaceRadius); //вроде считает
+    if (pos[2] < 0)
+        chi = 2 * Math.PI - chi;
+
+	var r_sphere, w_sphere;
+
+	//todo
+
+	const leftReferenceVector = vec4.fromValues(-r_sphere, 0, 0, w_sphere);
+	const rightReferenceVector = vec4.fromValues(r_sphere, 0, 0, w_sphere);
+
+	//todo
 }
