36e930380282093fabf7cf4278080e8ec2d4e0da
dmigranov <d.migranov@g.nsu.ru>
2022-04-05 15:10:55 +0700

    more params

--- selection_system.js
+++ selection_system.js
@@ -1,3 +1,6 @@
-export function raytraceSphereMouse() {
+// mouseX = (double)ms.x / width * 2. - 1.;
+// mouseY = -((double)ms.y / height * 2. - 1);
+
+export function raytraceSphereMouse(mouseX, mouseY, posWorld, spaceRadius, viewMatrix, projMatrix) {
 
 }
