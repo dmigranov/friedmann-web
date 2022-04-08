dc3135f5acb5208988457f9a09437edb1a8b9f2d
dmigranov <d.migranov@g.nsu.ru>
2022-04-08 12:03:47 +0700

    some

--- friedmann.js
+++ friedmann.js
@@ -649,9 +649,9 @@ function initScene(gl) {
 		else 
 		{
 			float darkenCoeff = 0.4f; //discrete 
-			float4 modifiedColorOriginal = float4(rgbNew.x, rgbNew.y, rgbNew.z, sourceColor.w);
-			float4 modifiedColorDarkened = (1 - darkenCoeff) * float4(0.f, 0.f, 0.f, 1.f) + darkenCoeff * modifiedColorOriginal;
+			vec4 dopplerColorDark = (1 - darkenCoeff) * vec4(0.f, 0.f, 0.f, 1.f) + darkenCoeff * dopplerColor;
 		
+
 			darkenedColor = vec4(0., 0., 0., 1.); //todo
 		}
 
