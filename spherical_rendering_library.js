d181bcdb4d406c3ca45614802c02003c5ceb55a7
dmigranov <d.migranov@g.nsu.ru>
2022-04-19 10:31:42 +0700

    some

--- spherical_rendering_library.js
+++ spherical_rendering_library.js
@@ -1,5 +1,12 @@
 
 
-function startGame(canvas) {
+function initializeEngine(canvas) {
+    const gl = canvas.getContext("webgl2");
+    gl.enable(gl.CULL_FACE); // should it stay? TODO
 
+
+}
+
+function startGame() {
+    //todo: main cycle
 }
