8296d19d4b8c71b769b359ce330d1bedd7f5ebcd
dmigranov <d.migranov@g.nsu.ru>
2022-05-25 12:22:13 +0700

    back to old dist calc

--- friedmann.js
+++ friedmann.js
@@ -317,8 +317,7 @@ function updatePage(scene, deltaTime) {
 
 			const referenceVectorNormalized = vec4.fromValues(0, rSphereNorm, 0, wSphereNorm);
 
-			//const sphereRadiusChi = Math.acos(referenceVectorNormalized[3]); //т.к. считаем от центра координат
-			const sphereRadiusChi = SphericalMath.sphericalDistance(referenceVectorNormalized, vec4.fromValues(0, 0, 0, 1), 1.);
+			const sphereRadiusChi = Math.acos(referenceVectorNormalized[3]); //т.к. считаем от центра координат
 
 			currentObjectOutput.innerHTML += ("<br/>Current sphere radius: " + sphereRadiusChi.toFixed(2));
 
